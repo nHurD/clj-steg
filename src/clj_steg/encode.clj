0708be45a569d16ded1274e7ffd9783ddaec6004
Jonathan Steele <jrsteele@gmail.com>
2015-01-19 15:36:20 -0500

    Minor Cleanup
    Moved the pixel setting code into its own private method

--- src/clj_steg/encode.clj
+++ src/clj_steg/encode.clj
@@ -1,6 +1,11 @@
 (ns clj-steg.encode
   (:require [clj-steg.image :as image]))
 
+(defn- apply-pixel [img x y data]
+  "Apply data to the given pixel at x and y"
+  (let [pixel (image/get-rgb-values img x y)]
+    (image/set-rgb-value img x y (assoc pixel :red data))))
+
 (defn encode-image [image message]
   "Encode the message into the image"
   (let [img (image/read-image image)
@@ -13,17 +18,11 @@
                     (conj (int \E)))]
         (loop [x 0 
                y 0 
-               pixel (image/get-rgb-values img x y) 
                current-char (first msg) 
                col (rest msg)]
           (if (nil? current-char)
             img
             (let [new-x (if (> (inc x) width) 0 (inc x))
                   new-y (if (> (inc x) width) (inc y) y)] 
-              (image/set-rgb-value img x y (assoc pixel :red current-char))
-              (recur 
-               new-x 
-               new-y 
-               (image/get-rgb-values img new-x new-y) 
-               (first col) 
-               (rest col)))))))))
+              (apply-pixel img x y current-char)
+              (recur new-x new-y (first col) (rest col)))))))))
